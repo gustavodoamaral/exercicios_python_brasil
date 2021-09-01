eb267915521f4cea0fe0eee10bf35357ff8dbc7d
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-09-01 10:49:21 -0300

    Miscellaneous Calculations

---
+++ EstrtuturaSequencial/EX11.py
@@ -0,0 +1,21 @@
+#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#o produto do dobro do primeiro com metade do segundo.
+#a soma do triplo do primeiro com o terceiro.
+#o terceiro elevado ao cubo.
+
+number1 = int(input('Enter an integer: '))
+number2 = int(input('Enter an integer: '))
+number3 = float(input('Enter a real number: '))
+
+double1_sum_half2 = (number1 * 2) + (number2/2)
+triple1_sum_3 = (number1 * 3) + number3
+third_cube = number3**3
+
+print(f'The product of double the first with half the second is: {double1_sum_half2}')
+print(f'The triple sum of the first and the third {triple1_sum_3}')
+print(f'the third to the cubethe third to the cube {third_cube}')
+
+
+
+
+
