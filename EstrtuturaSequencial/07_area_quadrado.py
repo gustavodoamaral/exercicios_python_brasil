03ed77c490dbc334025e2ec3acdfc9a8f844530e
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-09-02 10:44:16 -0300

    renamed

---
+++ EstrtuturaSequencial/07_area_quadrado.py
@@ -0,0 +1,6 @@
+altura = int(input('Informe a medida dos lados do quadrado: '))
+base = altura
+
+dobro_da_area = (base * altura) * 2
+
+print(f'O dobro da base de um quadrado de lados de {altura} cm² é {dobro_da_area} cm²')
