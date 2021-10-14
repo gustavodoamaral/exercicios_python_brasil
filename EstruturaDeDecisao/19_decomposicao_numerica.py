9c1ca52bdffa1532501c0c9487e13dfb47b379f8
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-10-14 12:26:39 -0300

    Exercícios Estrutura de Decisão

---
+++ EstruturaDeDecisao/19_decomposicao_numerica.py
@@ -0,0 +1,29 @@
+numero = int(input('Informe um número inteiro menor que 1000: '))
+
+centenas = int(numero / 100)
+unidades = int(numero % 10)
+dezenas = int((numero - unidades) / 10)
+
+saidac = str
+saidau = str
+saidad = str
+
+if centenas == 0 or centenas > 1:
+    saidac = f"{centenas} centenas"
+else: 
+    saidac = f"{centenas} centena"
+
+
+if dezenas == 0 or dezenas > 1:
+    saidad = f"{dezenas} dezenas"
+else: 
+    saidad = f"{dezenas} dezena"
+
+
+if unidades == 0 or unidades > 1:
+    saidau = f"{unidades} unidades"
+else: 
+    saidau = f"{unidades} unidade"
+
+
+print(f"{saidac}, {saidad} e {saidau}")
