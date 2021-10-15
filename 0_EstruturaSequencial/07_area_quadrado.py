4177a4abf2ecc3f4e78c1dddc52888f116e8b379
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-10-15 18:48:51 -0300

    Delete 0_EstruturaSequencial directory

--- 0_EstruturaSequencial/07_area_quadrado.py
+++
@@ -1,6 +0,0 @@
-altura = int(input('Informe a medida dos lados do quadrado: '))
-base = altura
-
-dobroArea = (base * altura) * 2
-
-print(f'O dobro da base de um quadrado de lados de {altura} cm² é {dobroArea} cm²')
--- 0_EstruturaSequencial/12_peso_ideal.py
+++
@@ -1,3 +0,0 @@
-altura = float(input('Informe sua altura para descobrir seu peso ideal: '))
-pesoIdeal = (72.7 * altura) - 58
-print(f'O peso ideal para a altura {altura} é: {pesoIdeal:.2f}')
